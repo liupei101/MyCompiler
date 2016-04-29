976806b589d18642ddf228ba7d3bbfb200314174
NUM_24 <1018810506@qq.com>
2016-04-29 21:15:15 +0800

    添加词法分析文件

---
+++ parser.cpp
@@ -0,0 +1,78 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include "parser.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void var_init()
+{
+
+}
+
+void advance()
+{
+
+}
+
+void var_add(int type)
+{
+
+}
+
+bool var_check()
+{
+    return true;
+}
+
+void A()
+{
+
+}
+
+void B()
+{
+
+}
+
+void C()
+{
+
+}
+
+void D()
+{
+
+}
+
+void E()
+{
+
+}
+
+void F()
+{
+
+}
+
+void G()
+{
+
+}
+
+void H()
+{
+
+}
+
+void I()
+{
+
+}
+
+void J()
+{
+
+}
